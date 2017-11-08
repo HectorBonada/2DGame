8918e1d18eecb8b89ed2c1df0c4d79bc746f09af
-Hector Bnada <hectorbonada@gmail.com>
2017-11-08 16:14:45 +0100

    Unity Up

---
+++ 2DGame/Assets/Scripts/Collisions2D.cs
@@ -0,0 +1,140 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collisions2D : MonoBehaviour
+{
+    [Header("State")]
+    [HideInInspector] public bool isGrounded;
+    [HideInInspector] public bool wasGroundedLastFrame;
+    [HideInInspector] public bool justGrounded;
+    [HideInInspector] public bool justNOTGrounded;
+    [HideInInspector] public bool isFalling;
+
+    [HideInInspector] public bool isHeadCollision;
+    [HideInInspector] public bool wasHeadCollisionLastFrame;
+    [HideInInspector] public bool justHeadCollision;
+    [HideInInspector] public bool justNOTHeadCollision;
+
+    [HideInInspector] public bool isLateralCollision;
+    [HideInInspector] public bool wasLateralCollisionLastFrame;
+    [HideInInspector] public bool justLateralCollision;
+    [HideInInspector] public bool justNOTLateralCollision;
+
+    [Header("Filter properties")]
+    public ContactFilter2D groundFilter;
+    public int maxHits;
+
+    [Header("Bottom box")]
+    public Vector2 groundBoxPosition;
+    public Vector2 groundBoxSize;
+    public bool checkGround;
+
+    [Header("Top box")]
+    public Vector2 headBoxPosition;
+    public Vector2 headBoxSize;
+    public bool checkHead;
+
+    [Header("Lateral box")]
+    public Vector2 lateralBoxPosition;
+    public Vector2 lateralBoxSize;
+    public bool checkLateral;
+
+
+    void Start ()
+    {
+        ResetState();
+	}
+    public void MyStart()
+    {
+
+    }
+
+    void ResetState()
+    {
+        wasGroundedLastFrame = isGrounded;
+
+        isGrounded = false;
+        justGrounded = false;
+        justNOTGrounded = false;
+        isFalling = true;
+
+        wasHeadCollisionLastFrame = isHeadCollision;
+        isHeadCollision = false;
+        justHeadCollision = false;
+        justNOTHeadCollision = false;
+
+        wasLateralCollisionLastFrame = isLateralCollision;
+        isLateralCollision = false;
+        justLateralCollision = false;
+        justNOTLateralCollision = false;
+
+    }
+
+    public void MyFixedUpdate()
+    {
+        ResetState();
+        if (checkGround) GroundDetection();    
+        if (checkHead) HeadDetection();
+        if (checkLateral) LateralDetection();
+    }
+
+    void GroundDetection()
+    {
+        Collider2D[] results = new Collider2D[maxHits];
+        Vector2 pos = this.transform.position;
+        int numHits = Physics2D.OverlapBox(groundBoxPosition + pos, groundBoxSize, 0, groundFilter, results);
+
+        if(numHits > 0)
+        {
+            isGrounded = true;
+        }
+
+        if(isGrounded) isFalling = false;
+        if(isGrounded && !wasGroundedLastFrame) justGrounded = true;
+        if(!isGrounded && wasGroundedLastFrame) justNOTGrounded = true;
+    }
+
+    void HeadDetection()
+    {
+        Collider2D[] results = new Collider2D[maxHits];
+        Vector2 pos = this.transform.position;
+        int numHits = Physics2D.OverlapBox(headBoxPosition + pos, headBoxSize, 0, groundFilter, results);
+
+        if (numHits > 0)
+        {
+            isHeadCollision = true;
+        }
+
+        if (isHeadCollision && !wasHeadCollisionLastFrame) justHeadCollision = true;
+        if (!isHeadCollision && wasHeadCollisionLastFrame) justNOTHeadCollision = true;
+    }
+
+    void LateralDetection()
+    {
+        Collider2D[] results = new Collider2D[maxHits];
+        Vector2 pos = this.transform.position;
+        int numHits = Physics2D.OverlapBox(lateralBoxPosition + pos, lateralBoxSize, 0, groundFilter, results);
+
+        if (numHits > 0)
+        {
+            isLateralCollision = true;
+        }
+
+        if (isLateralCollision && !wasLateralCollisionLastFrame) justLateralCollision = true;
+        if (!isLateralCollision && wasLateralCollisionLastFrame) justNOTLateralCollision = true;
+    }
+
+    public void Flip(bool face)
+    {
+        if(face) lateralBoxPosition.x = Mathf.Abs(lateralBoxPosition.x);
+    }
+    // Update is called once per frame
+    private void OnDrawGizmos()
+    {
+        Vector2 pos = this.transform.position;
+        Gizmos.DrawWireCube(groundBoxPosition + pos, groundBoxSize);
+        Gizmos.DrawWireCube(headBoxPosition + pos, headBoxSize);
+        Gizmos.DrawWireCube(lateralBoxPosition + pos, lateralBoxSize);
+    }
+}
---
+++ 2DGame/Assets/Scripts/Editor/Collisions2DInspector.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Collisions2D))]
+public class Collisions2DInspector : Editor
+{
+    static bool stateFoldout;
+    static bool drawDefaultInspector;
+
+    public override void OnInspectorGUI()
+    {
+        //base.OnInspectorGUI(); Si se deja escrito no afecta. Se visualiza de forma normal la informacion del inspector.
+        Collisions2D col = (Collisions2D)target;
+
+        GUIStyle booleanText = new GUIStyle();
+
+        EditorGUILayout.Space();
+        EditorGUI.indentLevel = 1;
+        stateFoldout = EditorGUILayout.Foldout(stateFoldout, "State", true, EditorStyles.toolbarDropDown);
+
+        //isGounded
+        //col.isGrounded = EditorGUILayout.Toggle("Is Grounded", col.isGrounded, EditorStyles.booleanText);
+
+        if(stateFoldout)
+        {
+            EditorGUI.indentLevel = 2;
+
+            EditorGUILayout.BeginVertical(EditorStyles.textArea);
+
+
+            if(col.isGrounded) booleanText.normal.textColor = Color.green;
+            else booleanText.normal.textColor = Color.red;
+            EditorGUILayout.LabelField("Is Ground", booleanText);
+
+            if(col.wasGroundedLastFrame) booleanText.normal.textColor = Color.green;
+            else booleanText.normal.textColor = Color.red;
+            EditorGUILayout.LabelField("Was Grounded Last Frame", booleanText);
+
+            if(col.justGrounded) booleanText.normal.textColor = Color.green;
+            else booleanText.normal.textColor = Color.red;
+            EditorGUILayout.LabelField("Just Grounded", booleanText);
+
+            if(col.justNOTGrounded) booleanText.normal.textColor = Color.green;
+            else booleanText.normal.textColor = Color.red;
+            EditorGUILayout.LabelField("Just NOT Grounded", booleanText);
+
+            if(col.isFalling) booleanText.normal.textColor = Color.green;
+            else booleanText.normal.textColor = Color.red;
+            EditorGUILayout.LabelField("Is Falling", booleanText);
+
+            if(col.isHeadCollision) booleanText.normal.textColor = Color.green;
+            else booleanText.normal.textColor = Color.red;
+            EditorGUILayout.LabelField("Is Head Collision", booleanText);
+
+            if(col.wasHeadCollisionLastFrame) booleanText.normal.textColor = Color.green;
+            else booleanText.normal.textColor = Color.red;
+            EditorGUILayout.LabelField("Was Head Collision Last Frame", booleanText);
+
+            if(col.justHeadCollision) booleanText.normal.textColor = Color.green;
+            else booleanText.normal.textColor = Color.red;
+            EditorGUILayout.LabelField("Just Head Collision", booleanText);
+
+            if(col.justNOTHeadCollision) booleanText.normal.textColor = Color.green;
+            else booleanText.normal.textColor = Color.red;
+            EditorGUILayout.LabelField("Just NOT Head Collision", booleanText);
+
+            if(col.isLateralCollision) booleanText.normal.textColor = Color.green;
+            else booleanText.normal.textColor = Color.red;
+            EditorGUILayout.LabelField("Is Lateral Collision", booleanText);
+
+            if(col.wasLateralCollisionLastFrame) booleanText.normal.textColor = Color.green;
+            else booleanText.normal.textColor = Color.red;
+            EditorGUILayout.LabelField("Was Lateral Collision Last Frame", booleanText);
+
+            if(col.justLateralCollision) booleanText.normal.textColor = Color.green;
+            else booleanText.normal.textColor = Color.red;
+            EditorGUILayout.LabelField("Just Lateral Collision", booleanText);
+
+            if(col.justNOTLateralCollision) booleanText.normal.textColor = Color.green;
+            else booleanText.normal.textColor = Color.red;
+            EditorGUILayout.LabelField("Just NOT Lateral Collision", booleanText);
+
+            EditorGUILayout.EndVertical();
+        }
+
+        EditorGUILayout.Space();
+
+        EditorGUI.indentLevel = 1;
+        drawDefaultInspector = EditorGUILayout.Foldout(drawDefaultInspector, "Default Inspector", true, EditorStyles.toolbarDropDown);
+        
+        if(drawDefaultInspector)
+        {
+            EditorGUI.indentLevel = 0;
+            EditorGUILayout.Space();
+        } 
+    }
+}
